ab2206f9e2b515f096a0687a1a61b7d7023a437c
Marek Rusinowski <marekrusinowski@gmail.com>
2024-05-07 21:08:05 +0200

    Add parsing of engine AutoHost interface packets
    In this commit I add parsing of the data that is send by engine
    to AutoHost interface in UDP packets. The parsing was tested on
    real packets gathered from live BAR servers.

---
+++ src/autohostInterface.test.ts
@@ -0,0 +1,287 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
+import {
+	parsePacket, EventType, LuaMsgScript, LuaMsgUIMode,
+	PacketParseError, LeaveReason, ReadyState, ChatDestination
+} from './autohostInterface.js';
+
+test('parse SERVER_STARTED', () => {
+	const event = parsePacket(Buffer.from('00', 'hex'));
+	assert.equal(event.type, EventType.SERVER_STARTED);
+
+	assert.throws(() => {
+		parsePacket(Buffer.from('0000', 'hex'));
+	}, PacketParseError);
+});
+
+test('parse SERVER_QUIT', () => {
+	const event = parsePacket(Buffer.from('01', 'hex'));
+	assert.equal(event.type, EventType.SERVER_QUIT);
+
+	assert.throws(() => {
+		parsePacket(Buffer.from('0100', 'hex'));
+	}, PacketParseError);
+});
+
+test('parse SERVER_STARTPLAYING', () => {
+	const event = parsePacket(Buffer.from('02a40000002e9836666a18a55fbcc6228ee62217492f686f6d652f73706164732f73706164732f7661722f436c75737465724d616e616765722f5b7465685d636c75737465725553345b31305d2f64656d6f732d7365727665722f323032342d30352d30345f32302d31382d35342d3236305f48656c6c617320426173696e2076315f3130352e312e312d323434392d6766313233346139204241523130352e7364667a', 'hex'));
+	assert.deepEqual(event, {
+		type: EventType.SERVER_STARTPLAYING,
+		gameId: '2e9836666a18a55fbcc6228ee6221749',
+		demoPath: '/home/spads/spads/var/ClusterManager/[teh]clusterUS4[10]/demos-server/2024-05-04_20-18-54-260_Hellas Basin v1_105.1.1-2449-gf1234a9 BAR105.sdfz'
+	});
+
+	assert.throws(() => {
+		parsePacket(Buffer.from('0200', 'hex'));
+	}, PacketParseError);
+
+	assert.throws(() => {
+		parsePacket(Buffer.from('02a40000002e9836666a18a55fbcc6228ee62217492f686f6d652f73706164732f73706164732f7661722f436c75737465724d616e616765722f5b7465685d636c75737465725553345b3130', 'hex'));
+	}, PacketParseError);
+});
+
+test('parse SERVER_GAMEOVER', () => {
+	const event = parsePacket(Buffer.from('03040601', 'hex'));
+	assert.deepEqual(event, {
+		type: EventType.SERVER_GAMEOVER,
+		player: 6,
+		winningAllyTeams: [1]
+	});
+
+	assert.throws(() => {
+		parsePacket(Buffer.from('030406', 'hex'));
+	}, PacketParseError);
+});
+
+test('parse SERVER_MESSAGE', () => {
+	const event = parsePacket(Buffer.from('04436f6e6e656374696e6720746f206175746f686f7374206f6e20706f7274203533313232', 'hex'));
+	assert.deepEqual(event, {
+		type: EventType.SERVER_MESSAGE,
+		message: 'Connecting to autohost on port 53122'
+	});
+});
+
+test('parse SERVER_WARNING', () => {
+	const event = parsePacket(Buffer.from('054f6e6c696e65207761726e696e67206c6f6c', 'hex'));
+	assert.deepEqual(event, {
+		type: EventType.SERVER_WARNING,
+		message: 'Online warning lol'
+	});
+});
+
+test('parse PLAYER_JOINED', () => {
+	const event = parsePacket(Buffer.from('0a0b417865', 'hex'));
+	assert.deepEqual(event, {
+		type: EventType.PLAYER_JOINED,
+		player: 11,
+		name: 'Axe'
+	});
+	assert.throws(() => {
+		parsePacket(Buffer.from('0a0b', 'hex'));
+	}, PacketParseError);
+});
+
+test('parse PLAYER_LEFT', () => {
+	assert.deepEqual(parsePacket(Buffer.from('0b1201', 'hex')), {
+		type: EventType.PLAYER_LEFT,
+		player: 18,
+		reason: LeaveReason.LEFT
+	});
+	assert.deepEqual(parsePacket(Buffer.from('0b0400', 'hex')), {
+		type: EventType.PLAYER_LEFT,
+		player: 4,
+		reason: LeaveReason.LOST_CONNECTION
+	});
+	assert.deepEqual(parsePacket(Buffer.from('0b1202', 'hex')), {
+		type: EventType.PLAYER_LEFT,
+		player: 18,
+		reason: LeaveReason.KICKED
+	});
+	assert.throws(() => {
+		parsePacket(Buffer.from('0b12', 'hex'));
+	}, PacketParseError);
+	assert.throws(() => {
+		parsePacket(Buffer.from('0b1203', 'hex'));
+	}, PacketParseError);
+});
+
+test('parse PLAYER_READY', () => {
+	assert.deepEqual(parsePacket(Buffer.from('0c0200', 'hex')), {
+		type: EventType.PLAYER_READY,
+		player: 2,
+		state: ReadyState.NOT_READY
+	});
+	assert.deepEqual(parsePacket(Buffer.from('0c0d01', 'hex')), {
+		type: EventType.PLAYER_READY,
+		player: 13,
+		state: ReadyState.READY
+	});
+	assert.deepEqual(parsePacket(Buffer.from('0c0d02', 'hex')), {
+		type: EventType.PLAYER_READY,
+		player: 13,
+		state: ReadyState.FORCED
+	});
+	assert.throws(() => {
+		parsePacket(Buffer.from('0c0d', 'hex'));
+	}, PacketParseError);
+	assert.throws(() => {
+		parsePacket(Buffer.from('0b1204', 'hex'));
+	}, PacketParseError);
+});
+
+test('parse PLAYER_CHAT', () => {
+	assert.deepEqual(parsePacket(Buffer.from('0d08fc6e696365', 'hex')), {
+		type: EventType.PLAYER_CHAT,
+		fromPlayer: 8,
+		destination: ChatDestination.TO_ALLIES,
+		message: 'nice'
+	});
+	assert.deepEqual(parsePacket(Buffer.from('0d0bfe72657369676e', 'hex')), {
+		type: EventType.PLAYER_CHAT,
+		fromPlayer: 11,
+		destination: ChatDestination.TO_EVERYONE,
+		message: 'resign'
+	});
+	assert.deepEqual(parsePacket(Buffer.from('0d11fd6c6f6c', 'hex')), {
+		type: EventType.PLAYER_CHAT,
+		fromPlayer: 17,
+		destination: ChatDestination.TO_SPECTATORS,
+		message: 'lol'
+	});
+	assert.deepEqual(parsePacket(Buffer.from('0d11016c6f6c', 'hex')), {
+		type: EventType.PLAYER_CHAT,
+		fromPlayer: 17,
+		toPlayer: 1,
+		destination: ChatDestination.TO_PLAYER,
+		message: 'lol'
+	});
+	assert.throws(() => {
+		parsePacket(Buffer.from('0d11', 'hex'));
+	}, PacketParseError);
+});
+
+test('parse PLAYER_DEFEATED', () => {
+	assert.deepEqual(parsePacket(Buffer.from('0e0b', 'hex')), {
+		type: EventType.PLAYER_DEFEATED,
+		player: 11,
+	});
+	assert.throws(() => {
+		parsePacket(Buffer.from('0e', 'hex'));
+	}, PacketParseError);
+});
+
+test('parse GAME_LUAMSG', () => {
+	const event1 = parsePacket(Buffer.from('14320c000a640000407a683630', 'hex'));
+	assert.deepEqual(event1, {
+		type: EventType.GAME_LUAMSG,
+		script: LuaMsgScript.RULES,
+		player: 10,
+		data: Buffer.from('407a683630', 'hex')
+	});
+
+	const event2 = parsePacket(Buffer.from('1432180000d0070044726166744f726465725f52616e646f6d', 'hex'));
+	assert.deepEqual(event2, {
+		type: EventType.GAME_LUAMSG,
+		script: LuaMsgScript.UI,
+		uiMode: LuaMsgUIMode.ALL,
+		player: 0,
+		data: Buffer.from('DraftOrder_Random'),
+	});
+
+	assert.throws(() => {
+		parsePacket(Buffer.from('143200', 'hex'));
+	}, PacketParseError);
+
+	assert.throws(() => {
+		parsePacket(Buffer.from('14330c000a640000407a683630', 'hex'));
+	}, PacketParseError);
+});
+
+test('parse GAME_TEAMSTAT', () => {
+	const event = parsePacket(Buffer.from('3c0700e10000ade0ad470abc944a8c38d747b4138f4a000000004a052144a2208244d308ca48f19406461fdc5148da6d3f478c976947f50100000e010000010000000600000000000000000000004c000000', 'hex'));
+	assert.deepEqual(event, {
+		type: EventType.GAME_TEAMSTAT,
+		teamNumber: 7,
+		// I hope that floating point numbers in decimal are precise enough
+		// for this test to work reliably...
+		stats: {
+			frame: 57600,
+			metalUsed: 89025.3515625,
+			energyUsed: 4873733,
+			metalProduced: 110193.09375,
+			energyProduced: 4688346,
+			metalExcess: 0,
+			energyExcess: 644.0826416015625,
+			metalReceived: 1041.019775390625,
+			energyReceived: 413766.59375,
+			metalSent: 8613.2353515625,
+			energySent: 214896.484375,
+			damageDealt: 49005.8515625,
+			damageReceived: 59799.546875,
+			unitsProduced: 501,
+			unitsDied: 270,
+			unitsReceived: 1,
+			unitsSent: 6,
+			unitsCaptured: 0,
+			unitsOutCaptured: 0,
+			unitsKilled: 76
+		}
+	});
+	assert.throws(() => {
+		parsePacket(Buffer.from('3c0700e10000ade0', 'hex'));
+	}, PacketParseError);
+});
+
+
+/**
+ * You can dump the network traffic with tcpdump:
+ *
+ *   sudo tcpdump -i lo -w autohost.pcap udp portrange 53100-53250
+ *
+ * and then use the code similar to below to analyze the pcap file.
+
+import pcap from 'pcap';
+import util from 'node:util';
+
+const session = pcap.createOfflineSession('/path/to/autohost.pcap.pcap');
+
+let count = 0;
+let totalCount = 0;
+const typeMap = new Map();
+
+session.on('packet', (rawPacket) => {
+	const packet = pcap.decode.packet(rawPacket);
+	totalCount++;
+
+	const sport = packet.payload.payload.payload.sport;
+	if (sport >= 53100 && sport <= 53250) {
+		return;
+	}
+
+	const decoded = parsePacket(packet.payload.payload.payload.data);
+	typeMap.set(decoded.type, (typeMap.get(decoded.type) || 0) + 1);
+
+
+	// Filter out some events
+	if (decoded.type != EventType.PLAYER_JOINED) {
+		return;
+	}
+	count++;
+	if (count >= 100) {
+		session.close();
+	}
+
+	console.log(EventType[decoded.type], decoded);
+	console.log(packet.payload.payload.payload.data.toString('hex'));
+	console.log(util.inspect(packet.payload.payload.payload, { depth: null }));
+});
+
+session.on('complete', () => {
+	console.log('total:', totalCount);
+	Array.from(typeMap.keys()).sort((a, b) => a - b).forEach((key) => {
+		console.log(`  ${EventType[key]}[${key}] => ${typeMap.get(key)}`);
+	});
+});
+
+*/
---
+++ src/autohostInterface.ts
@@ -0,0 +1,490 @@
+/**
+ * This module provides a parser for the SpringRTS/Recoil autohost interface packets.
+ *
+ * Unfortunately, the autohost interface is not documented, so the parser is based on
+ * the implementation in the engine code and validation against actual packets
+ * in the wild in BAR. Even for the pieces that are documented, the documentation is
+ * often incorrect or incomplete.
+ */
+
+/**
+ * This enum values are from rts/Net/AutohostInterface.cpp and the comments are
+ * fixed to match the reality.
+ *
+ * Each packet from engine to autohost interface starts with a byte that
+ * indicates the type of the message, followed by the message-specific data.
+ * In the documentation the message-specific data is described as a list of
+ * fields, in brackets.
+ *
+ * char[] are *not* delimited in any way e.g. by '\0', so they can be used only
+ * as a last element of the UDP packet.
+ */
+export enum EventType {
+	/**
+	 * Server has started
+	 *
+	 *   ()
+	 */
+	SERVER_STARTED = 0,
+
+	/**
+	 * Server is about to exit
+	 *
+	 *   ()
+	 */
+	SERVER_QUIT = 1,
+
+	/**
+	 * Game starts
+	 *
+	 *   (uint32 msgsize, uint8[16] gameId, char[] demoName)
+	 */
+	SERVER_STARTPLAYING = 2,
+
+	/**
+	 * Game has ended
+	 *
+	 *   (uint8 player, uint8 msgsize, uint8[msgsize - 3] winningAllyTeamss)
+	 */
+	SERVER_GAMEOVER = 3,
+
+	/**
+	 * An information message from server
+	 *
+	 *   (char[] message)
+	 */
+	SERVER_MESSAGE = 4,
+
+	/**
+	 * A warning message from server
+	 *
+	 *   (char[] warningmessage)
+	 */
+	SERVER_WARNING = 5,
+
+	/**
+	 * Player has joined the game
+	 *
+	 *   (uint8 playernumber, char[] name)
+	 */
+	PLAYER_JOINED = 10,
+
+	/**
+	 * Player has left
+	 *
+	 *   (uint8 playernumber, uint8 reason)
+	 *
+	 * Reason:
+	 *   - 0: lost connection
+	 *   - 1: left
+	 *   - 2: kicked
+	 */
+	PLAYER_LEFT = 11,
+
+	/**
+	 * Player has updated its ready-state
+	 *
+	 *   (uint8 playernumber, uint8 state)
+	 *
+	 * State:
+	 *   - 0: not ready
+	 *   - 1: ready
+	 *   - 2: forced
+	 *   - 3: failed to ready (in engine code it says it's not clear if possible)
+	 */
+	PLAYER_READY = 12,
+
+	/**
+	 * Player has sent a chat message
+	 *
+	 *   (uint8 playernumber, uint8 destination, char[] text)
+	 *
+	 * Destination can be any of:
+	 *   - a playernumber
+	 *   - TO_ALLIES = 252
+	 *   - TO_SPECTATORS = 253
+	 *   - TO_EVERYONE = 254
+	 *
+	 * Enum values from rts/Game/ChatMessage.h, value 255 is a SERVER_PLAYER
+	 * but in this context, it's meaningless, server doesn't do anything with
+	 * them, messages that don't match any player are not used for anything and
+	 * nobody sees them.
+	 */
+	PLAYER_CHAT = 13,
+
+	/**
+	 * Player has been defeated
+	 *
+	 *   (uint8 playernumber)
+	 */
+	PLAYER_DEFEATED = 14,
+
+	/**
+	 * Message sent by Lua script
+	 *
+	 *   (uint8 magic = 50, uint16 msgsize, uint8 playernumber, uint16 script, uint8 uiMode, uint8[msgsize - 8] data)
+	 *
+	 * The message data is a straight copy of the whole NETMSG_LUAMSG packet
+	 * including the magic 50 byte. Take a look at the engine code for
+	 * CBaseNetProtocol::SendLuaMsg function and all of it's callers to see
+	 * how it's constructed.
+	 */
+	GAME_LUAMSG = 20,
+
+	/**
+	 * Team statistics
+	 *
+	 *   (uint8 teamnumber, TeamStatistics stats)
+	 *
+	 * TeamStatistics is object as defined in rts/Sim/Misc/TeamStatistics.h
+	 */
+	GAME_TEAMSTAT = 60,
+}
+
+export interface EvServerStarted {
+	type: EventType.SERVER_STARTED;
+}
+
+export interface EvServerQuit {
+	type: EventType.SERVER_QUIT;
+}
+
+export interface EvServerStartPlaying {
+	type: EventType.SERVER_STARTPLAYING;
+	gameId: string;
+	demoPath: string;
+}
+
+export interface EvServerGameOver {
+	type: EventType.SERVER_GAMEOVER;
+	player: number;
+	winningAllyTeams: number[];
+}
+
+export interface EvServerMessage {
+	type: EventType.SERVER_MESSAGE;
+	message: string;
+}
+
+export interface EvServerWarning {
+	type: EventType.SERVER_WARNING;
+	message: string;
+}
+
+export interface EvPlayerJoined {
+	type: EventType.PLAYER_JOINED;
+	player: number;
+	name: string;
+}
+
+export enum LeaveReason {
+	LOST_CONNECTION = 0,
+	LEFT = 1,
+	KICKED = 2,
+}
+
+export interface EvPlayerLeft {
+	type: EventType.PLAYER_LEFT;
+	player: number;
+	reason: LeaveReason;
+}
+
+// This is based on enum values in rts/Game/Players/Player.h
+// For all BAR logs, the only values I saw are NOT_READY and FORCED.
+export enum ReadyState {
+	NOT_READY = 0,
+	READY = 1,
+	FORCED = 2,
+	FAILED = 3,
+}
+
+export interface EvPlayerReady {
+	type: EventType.PLAYER_READY;
+	player: number;
+	state: ReadyState;
+}
+
+// Enum values from rts/Game/ChatMessage.h
+export enum ChatDestination {
+	TO_PLAYER = 0,
+	TO_ALLIES = 252,
+	TO_SPECTATORS = 253,
+	TO_EVERYONE = 254,
+}
+
+export interface EvPlayerChat {
+	type: EventType.PLAYER_CHAT;
+	fromPlayer: number;
+
+	/**
+	 * If destination is TO_PLAYER, this field is set.
+	 */
+	toPlayer?: number;
+	destination: ChatDestination;
+	message: string;
+}
+
+export interface EvPlayerDefeated {
+	type: EventType.PLAYER_DEFEATED;
+	player: number;
+}
+
+// The allowed values are determined from all the calls to
+// CBaseNetProtocol::SendLuaMsg function in engine.
+export enum LuaMsgScript {
+	UI = 2000,
+	GAIA = 300,
+	RULES = 100,
+}
+
+// From implementation of LuaUnsyncedCtrl::SendLuaUIMsg.
+export enum LuaMsgUIMode {
+	ALL = 0,
+	ALLIES = 0x61, // 'a'
+	SPECTATORS = 0x73, // 's'
+}
+
+export interface EvGameLuaMsg {
+	type: EventType.GAME_LUAMSG;
+	player: number;
+	script: LuaMsgScript;
+
+	/**
+	 * Field set only when script is LuaMsgScript.UI
+	 */
+	uiMode?: LuaMsgUIMode;
+
+	/**
+	 * What can be set in the bugger is determined entirely by the game.
+	 *
+	 * For example
+	 * https://github.com/Jazcash/sdfz-demo-parser/blob/1068cafd49f2eca49c06874ab004a8408ed783f1/src/lua-parser.ts
+	 * has some examples of what can be here and how to parse it.
+	 */
+	data: Buffer;
+}
+
+// From rts/Sim/Misc/TeamStatistics.h
+export interface TeamStatistics {
+	frame: number;
+	metalUsed: number;
+	energyUsed: number;
+	metalProduced: number;
+	energyProduced: number;
+	metalExcess: number;
+	energyExcess: number;
+	metalReceived: number;
+	energyReceived: number;
+	metalSent: number;
+	energySent: number;
+	damageDealt: number;
+	damageReceived: number;
+	unitsProduced: number;
+	unitsDied: number;
+	unitsReceived: number;
+	unitsSent: number;
+	unitsCaptured: number;
+	unitsOutCaptured: number;
+	unitsKilled: number;
+}
+
+export interface EvGameTeamStat {
+	type: EventType.GAME_TEAMSTAT;
+	teamNumber: number;
+	stats: TeamStatistics;
+}
+
+export type Event = EvServerStarted | EvServerQuit | EvServerStartPlaying
+	| EvServerGameOver | EvServerMessage | EvServerWarning | EvPlayerJoined
+	| EvPlayerLeft | EvPlayerReady | EvPlayerChat | EvPlayerDefeated
+	| EvGameLuaMsg | EvGameTeamStat;
+
+export class PacketParseError extends Error {
+	constructor(msg: string) {
+		super(msg);
+		this.name = 'DecodeError';
+	}
+}
+
+/**
+ * Parse a packet from the autohost interface.
+ *
+ * @param msg The packet to parse
+ * @returns The parsed event object
+ * @throws PacketParseError if the packet is invalid
+ */
+export function parsePacket(msg: Buffer): Event {
+	if (msg.length < 1) {
+		throw new PacketParseError('Empty packet');
+	}
+	const type = msg.readUInt8(0);
+	switch (type) {
+		case EventType.SERVER_STARTED:
+			if (msg.length != 1) {
+				throw new PacketParseError('SERVER_STARTED: invalid message length');
+			}
+			return { type };
+		case EventType.SERVER_QUIT:
+			if (msg.length != 1) {
+				throw new PacketParseError('SERVER_QUIT: invalid message length');
+			}
+			return { type };
+		case EventType.SERVER_STARTPLAYING: {
+			if (msg.length < 5 + 16) {
+				throw new PacketParseError('SERVER_STARTPLAYING: message too short');
+			}
+			const msgSize = msg.readUint32LE(1);
+			if (msgSize != msg.length) {
+				throw new PacketParseError('SERVER_STARTPLAYING: msgSize does not match message length');
+			}
+			return {
+				type,
+				gameId: msg.toString('hex', 5, 5 + 16),
+				demoPath: msg.toString('utf8', 5 + 16),
+			};
+		}
+		case EventType.SERVER_GAMEOVER: {
+			if (msg.length < 3) {
+				throw new PacketParseError('SERVER_GAMEOVER: message too short');
+			}
+			const msgSize = msg.readUInt8(1);
+			if (msgSize != msg.length) {
+				throw new PacketParseError('SERVER_GAMEOVER: msgSize does not match message length');
+			}
+			const winningAllyTeams = new Array<number>(msgSize - 3);
+			for (let i = 0; i < msgSize - 3; i++) {
+				winningAllyTeams[i] = msg.readUInt8(3 + i);
+			}
+			return { type, player: msg.readUInt8(2), winningAllyTeams };
+		}
+		case EventType.SERVER_MESSAGE:
+			return { type, message: msg.toString('utf8', 1) };
+		case EventType.SERVER_WARNING:
+			return { type, message: msg.toString('utf8', 1) };
+		case EventType.PLAYER_JOINED:
+			if (msg.length < 3) {
+				throw new PacketParseError('PLAYER_JOINED: message too short');
+			}
+			return { type, player: msg.readUInt8(1), name: msg.toString('utf8', 2) };
+		case EventType.PLAYER_LEFT: {
+			if (msg.length != 3) {
+				throw new PacketParseError('PLAYER_LEFT: invalid message length');
+			}
+			const reason = msg.readUInt8(2);
+			if (reason > 2) {
+				throw new PacketParseError(`PLAYER_LEFT: invalid leave reason: ${reason}`);
+			}
+			return { type, player: msg.readUInt8(1), reason: reason as LeaveReason };
+		}
+		case EventType.PLAYER_READY: {
+			if (msg.length != 3) {
+				throw new PacketParseError('PLAYER_READY: invalid message length');
+			}
+			const state = msg.readUInt8(2);
+			if (state > 3) {
+				throw new PacketParseError(`PLAYER_READY: invalid ready state: ${state}`);
+			}
+			return { type, player: msg.readUInt8(1), state: state as ReadyState };
+		}
+		case EventType.PLAYER_CHAT: {
+			if (msg.length < 3) {
+				throw new PacketParseError('PLAYER_CHAT: message too short');
+			}
+			const destination = msg.readUInt8(2);
+			// We split the destination into two fields for easier processing.
+			let destinationType: ChatDestination;
+			let toPlayer: number | undefined;
+			if (destination == ChatDestination.TO_ALLIES
+				|| destination == ChatDestination.TO_SPECTATORS
+				|| destination == ChatDestination.TO_EVERYONE) {
+				destinationType = destination;
+			} else {
+				toPlayer = destination;
+				destinationType = ChatDestination.TO_PLAYER;
+			}
+			const res: EvPlayerChat = {
+				type,
+				fromPlayer: msg.readUInt8(1),
+				destination: destinationType,
+				message: msg.toString('utf8', 3)
+			};
+			if (toPlayer !== undefined) {
+				res.toPlayer = toPlayer;
+			}
+			return res;
+		}
+		case EventType.PLAYER_DEFEATED:
+			if (msg.length != 2) {
+				throw new PacketParseError('PLAYER_DEFEATED: invalid message length');
+			}
+			return { type, player: msg.readUInt8(1) };
+		case EventType.GAME_LUAMSG: {
+			if (msg.length < 8) {
+				throw new PacketParseError('GAME_LUAMSG: message too short');
+			}
+			const packetType = msg.readUInt8(1);
+			if (packetType != 50) {
+				throw new PacketParseError(`GAME_LUAMSG: invalid packet type: ${packetType}`);
+			}
+			const packetSize = msg.readUInt16LE(2);
+			if (packetSize != msg.length - 1) {
+				throw new PacketParseError('GAME_LUAMSG: packet size does not match message length');
+			}
+			const script = msg.readUInt16LE(5);
+			if (script != LuaMsgScript.UI
+				&& script != LuaMsgScript.GAIA
+				&& script != LuaMsgScript.RULES) {
+				throw new PacketParseError(`GAME_LUAMSG: invalid script: ${script}`);
+			}
+			const res: EvGameLuaMsg = {
+				type,
+				player: msg.readUInt8(4),
+				script,
+				data: msg.subarray(8),
+			};
+			const uiMode = msg.readUInt8(7);
+			if (script == LuaMsgScript.UI) {
+				if (uiMode != LuaMsgUIMode.ALL
+					&& uiMode != LuaMsgUIMode.ALLIES
+					&& uiMode != LuaMsgUIMode.SPECTATORS) {
+					throw new PacketParseError(`GAME_LUAMSG: invalid UI mode: ${uiMode}`);
+				}
+				res.uiMode = uiMode;
+			} else if (uiMode != 0) {
+				throw new PacketParseError(
+					`GAME_LUAMSG: expected mode 0 for ${LuaMsgScript[script]}, got ${uiMode}`);
+			}
+			return res;
+		}
+		case EventType.GAME_TEAMSTAT: {
+			if (msg.length != 82) {
+				throw new PacketParseError(`GAME_TEAMSTAT: invalid message length (${msg.length})`);
+			}
+			let off;
+			const stats: TeamStatistics = {
+				frame: msg.readInt32LE(off = 2),
+				metalUsed: msg.readFloatLE(off += 4),
+				energyUsed: msg.readFloatLE(off += 4),
+				metalProduced: msg.readFloatLE(off += 4),
+				energyProduced: msg.readFloatLE(off += 4),
+				metalExcess: msg.readFloatLE(off += 4),
+				energyExcess: msg.readFloatLE(off += 4),
+				metalReceived: msg.readFloatLE(off += 4),
+				energyReceived: msg.readFloatLE(off += 4),
+				metalSent: msg.readFloatLE(off += 4),
+				energySent: msg.readFloatLE(off += 4),
+				damageDealt: msg.readFloatLE(off += 4),
+				damageReceived: msg.readFloatLE(off += 4),
+				unitsProduced: msg.readInt32LE(off += 4),
+				unitsDied: msg.readInt32LE(off += 4),
+				unitsReceived: msg.readInt32LE(off += 4),
+				unitsSent: msg.readInt32LE(off += 4),
+				unitsCaptured: msg.readInt32LE(off += 4),
+				unitsOutCaptured: msg.readInt32LE(off += 4),
+				unitsKilled: msg.readInt32LE(off += 4),
+			};
+			return { type, teamNumber: msg.readUInt8(1), stats };
+		}
+	}
+	throw new PacketParseError(`Unknown event type: ${type}`);
+}
