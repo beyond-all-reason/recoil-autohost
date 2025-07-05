08b5da8c54f03e2993ff9d6588396098c882ebc4
Marek Rusinowski <marekrusinowski@gmail.com>
2025-07-05 18:24:01 +0200

    Don't lint files inside of dist output directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,27 +4,32 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 
-export default tseslint.config({
-	files: ['src/**/*.ts'],
-	extends: [eslint.configs.recommended, tseslint.configs.recommended, prettier],
-	rules: {
-		'@typescript-eslint/naming-convention': [
-			'error',
-			{
-				'selector': ['parameter', 'variable'],
-				'leadingUnderscore': 'require',
-				'format': ['camelCase'],
-				'modifiers': ['unused'],
-			},
-		],
-		'@typescript-eslint/no-unused-vars': [
-			'error',
-			{
-				'args': 'all',
-				'argsIgnorePattern': '^_',
-				'varsIgnorePattern': '^_',
-				'caughtErrorsIgnorePattern': '^_',
-			},
-		],
+export default tseslint.config(
+	{
+		ignores: ['dist'],
 	},
-});
+	{
+		files: ['src/**/*.ts'],
+		extends: [eslint.configs.recommended, tseslint.configs.recommended, prettier],
+		rules: {
+			'@typescript-eslint/naming-convention': [
+				'error',
+				{
+					'selector': ['parameter', 'variable'],
+					'leadingUnderscore': 'require',
+					'format': ['camelCase'],
+					'modifiers': ['unused'],
+				},
+			],
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					'args': 'all',
+					'argsIgnorePattern': '^_',
+					'varsIgnorePattern': '^_',
+					'caughtErrorsIgnorePattern': '^_',
+				},
+			],
+		},
+	},
+);
